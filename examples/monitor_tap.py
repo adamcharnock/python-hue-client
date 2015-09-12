88099f9bcf2305e39251695ee6be1f2f6ad827a6
Adam Charnock <adam@adamcharnock.com>
2015-09-12 13:14:26 +0100

    Creating examples directory with hue tap monitoring sample

--- examples/monitor_tap.py
+++ examples/monitor_tap.py
@@ -6,10 +6,9 @@ if __name__ == '__main__':
     def handle(resource, field, previous, current):
         print "Change from {} to {}".format(previous, current)
 
+    hue_api.authenticate_interactive(app_name='Tap Test')
+
     switch = TapSwitch.objects.get(id=3)
     switch.monitor(lambda sw: sw.state.as_dict(), handle, poll_interval=0.2)
-
-    light = Light.objects.get(id=1)
-    light.monitor(lambda l: l.state.on, handle, poll_interval=0.2)
 
     hue_api.start_monitor_loop()
