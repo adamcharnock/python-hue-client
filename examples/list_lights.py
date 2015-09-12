fe37eee353bc73f497af9a1106b7a289ff769d67
Adam Charnock <adam@adamcharnock.com>
2015-09-12 13:24:39 +0100

    Adding list_lights example

---
+++ examples/list_lights.py
@@ -0,0 +1,35 @@
+from pprint import pprint
+from hueclient.api import hue_api
+from hueclient.models.light import Light
+from hueclient.models.sensors import TapSwitch, TapSwitchState
+
+if __name__ == '__main__':
+    hue_api.authenticate_interactive(app_name='List Lights Example')
+
+    # Show a summary of all lights first
+    for light in Light.objects.all():
+        print(
+            "Light {id} is named '{name}' and is {onoff} (brightness: {brightness})".format(
+                id=light.id,
+                name=light.name,
+                onoff='on' if light.state.on else 'off',
+                brightness=light.state.brightness,
+            )
+        )
+
+    while True:
+        # Let's offer the user some more detailed information
+        light_id = raw_input("Enter a light ID for more information (enter to exit): ")
+        if not light_id:
+            print('Bye!')
+            exit()
+
+        # Get the light using the specified ID
+        light = Light.objects.get(id=light_id)
+
+        # Get the information as a dictionary using as_dict()
+        light_as_dict = light.as_dict()
+        light_as_dict['state'] = light.state.as_dict()
+
+        # Prettily print it
+        pprint(light_as_dict)
