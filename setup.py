c708621bde36c6dab5fccc5678a83dec06641d45
Adam Charnock <adam@adamcharnock.com>
2015-09-16 00:24:33 +0100

    Version bump to 0.1.0 and updating CHANGES.txt

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from os.path import exists
 
 from setuptools import setup, find_packages
 
-from hueclient import __version__
 
 setup(
     name='python-hue-client',
-    version=__version__,
+    version=open('VERSION').read().strip(),
     # Your name & email here
     author='Adam Charnock',
     author_email='adam@adamcharnock.com',
